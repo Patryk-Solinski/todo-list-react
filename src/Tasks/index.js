8d3bbf18f3f1acb9a2bfc3f3a04e4c8d45dbaa36
Patryk Solinski <solinskipatryk8@gmail.com>
2023-01-06 18:17:16 +0100

    check list without interations yet

---
+++ src/Tasks/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./style.css";
+
+const Tasks = ({tasks,hideDoneTasks}) => (
+    <ul className="tasks">
+        {tasks.map(task => (
+            <li
+                key={task.id}
+                className={`tasks__item${task.done && hideDoneTasks ? "list__tasks--hidden" : ""}`}>
+
+                <button
+                    className="task__button task__button--done">
+                    {task.done ? "✓" : " "}
+                </button>
+
+                <span className={`tasks__content ${task.done ? "tasks__content--done" : ""}`}>
+                    {task.content}
+                </span>
+
+                <button className="task__button task__button--remove"> 🗑
+                </button>
+
+                
+            </li>
+        ))}
+    </ul>
+
+);
+
+export default Tasks;
