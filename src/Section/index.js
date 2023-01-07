a8885798a5991bb40785c5f46f1ecab75fbae83d
Patryk Solinski <solinskipatryk8@gmail.com>
2023-01-07 19:37:50 +0100

    removed className section__header--grid

--- src/Section/index.js
+++ src/Section/index.js
@@ -2,7 +2,7 @@ import "./style.css";
 
 const Section = ({ title, body, extraHeaderContent }) => (
     <section className="section">
-        <header className="section__header section__header--grid">
+        <header className="section__header">
             <h2 className="section__header--title">{title}</h2>
             {extraHeaderContent}
         </header>
