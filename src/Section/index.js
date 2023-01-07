ac00cda4ad438f4040881ee239463839a5573935
Patryk Solinski <solinskipatryk8@gmail.com>
2023-01-07 19:34:18 +0100

    changed name of class

--- src/Section/index.js
+++ src/Section/index.js
@@ -1,13 +1,15 @@
 import "./style.css";
 
-const Section = ({ title, body,extraHeaderContent }) => (
+const Section = ({ title, body, extraHeaderContent }) => (
     <section className="section">
         <header className="section__header section__header--grid">
-            <h2 className="section__h2">{title}</h2>
-           {extraHeaderContent}
+            <h2 className="section__header--title">{title}</h2>
+            {extraHeaderContent}
         </header>
+        <div>
+            {body}
+        </div>
     </section>
-
 );
 
 export default Section;
