9e28454a1df9c5834f1420906c2aa19bfed37318
Patryk Solinski <solinskipatryk8@gmail.com>
2023-01-20 21:45:42 +0100

    removed tasks

--- src/App.js
+++ src/App.js
@@ -8,10 +8,7 @@ import Container from './Container';
 
 function App() {
   const [hideDone, setHideDone] = useState(false);
-  const [tasks, setTasks] = useState([
-    { id: 1, content: "przejść na reacta", done: false },
-    { id: 2, content: "kupić pomarańcze ", done: true },
-  ]);
+  const [tasks, setTasks] = useState([]);
 
   const toggleTaskDone = (id) => {
     setTasks(tasks => tasks.map( task=>task.id === id ? 
