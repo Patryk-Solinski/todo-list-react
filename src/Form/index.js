d80ab8f29283cd08572df6fb7232244a1d7a3553
Patryk Solinski <solinskipatryk8@gmail.com>
2023-02-01 18:47:36 +0100

    added focus to Field

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,28 +1,35 @@
-import { TasksForm, Field, Button} from "./styled";
-import { useState } from "react";
+import { TasksForm, Field, Button } from "./styled";
+import { useState, useRef } from "react";
 
 const Form = ({ addNewTask }) => {
-    const [newTaskContent, setNewTaskContent] = useState("");
+  const [newTaskContent, setNewTaskContent] = useState("");
 
-    const onFormSubmit = (event) => {
-        event.preventDefault();
-        if (newTaskContent.trim() === "") {
-            return;
-        }
-        
-        addNewTask(newTaskContent.trim());
-        setNewTaskContent("")
-    };
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    if (newTaskContent.trim() === "") {
+      return;
+    }
+
+    addNewTask(newTaskContent.trim());
+    setNewTaskContent("")
+  };
+  const inputRef = useRef(null);
+  const focusInput = () => {
+    inputRef.current.focus();
+  };
 
   return (
     <TasksForm onSubmit={onFormSubmit}>
       <Field
+        ref={inputRef}
         value={newTaskContent}
         placeholder="Co jest do zrobienia?"
         onChange={({ target }) => setNewTaskContent(target.value)} />
-      <Button>Dodaj zadanie</Button>
+      <Button onClick={focusInput}>
+        Dodaj zadanie
+      </Button>
     </TasksForm>
   );
 };
 
-  export default Form;
+export default Form;
