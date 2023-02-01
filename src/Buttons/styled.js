cf8f9693e189cef997fb2bacc6b62c6350e17ba0
Patryk Solinski <solinskipatryk8@gmail.com>
2023-02-01 18:24:17 +0100

    added styles to Components

---
+++ src/Buttons/styled.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const ListButtons = styled.div`
+    display: grid;
+    grid-template-columns: auto auto;
+    grid-gap: 20px;
+
+    @media (max-width: 767px){
+        grid-template-columns: auto;
+        justify-content: center;
+        grid-gap: 15px;
+    }
+`;
+
+export const LButton = styled.button`
+    border: none;
+    background-color: transparent;
+    color: hsl(180, 100%, 25%);
+    transition: color 0.8s;
+    font-size: 15px;
+
+    & :hover {
+        color: hsl(180, 100%, 35%);
+    }
+
+    & :disabled {
+        color: #ccc;
+    }
+`;
