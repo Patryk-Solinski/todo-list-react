bba8ca7ac2f746ab7221edd9f994b6f49b5f0134
Patryk Solinski <solinskipatryk8@gmail.com>
2023-02-01 14:21:02 +0100

    added stless to Components

---
+++ src/Section/styled.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const TasksSection = styled.section`
+    background: white;
+    margin-bottom: 10px 0;
+    box-shadow: 0 0 5px #ddd;
+    padding: 15px;
+    margin-bottom: 10px;
+`;
+
+export const Header = styled.header`
+    font-size: 20px;
+    padding: 20px;
+    margin: 0;
+    border-bottom: 1px solid #ddd;
+    display: grid;
+    grid-gap: 20px;
+    grid-template-columns: auto auto;
+    justify-content: space-between;
+    align-items: center;
+
+    @media (max-width: 767px) {
+        grid-template-columns: 1fr;
+    }
+`;
+
+export const Title =styled.h2`
+    padding: 0;
+    font-size: 20px;
+    margin: 0;
+`;
