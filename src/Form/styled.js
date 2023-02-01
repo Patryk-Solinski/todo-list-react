bbc786f113b20886d657bf52f170a9871cab9d61
Patryk Solinski <solinskipatryk8@gmail.com>
2023-02-01 14:07:07 +0100

    added styles to TaskForm, Field, Button

---
+++ src/Form/styled.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const TasksForm = styled.form`
+    display: grid;
+    grid-template-columns: 1fr auto;
+    grid-gap: 20px;
+    padding: 20px;
+
+    @media (max-width: 767px){
+        grid-template-columns: 1fr;
+        padding-bottom: 20px;
+    }
+`;
+
+export const Field = styled.input`
+    padding: 10px;
+    border: 1px solid hsl(0, 10%, 89%);
+`;
+
+export const Button = styled.button`
+    border: none;
+    background-color: teal;
+    color: white;
+    padding: 10px;
+    transition: 0.7s;
+
+    & :hover {
+        background-color: hsl(180, 100%, 30%);
+        transform: scale(1.2);
+    }
+
+    & :active {
+        background-color: hsl(180, 100%, 35%);
+    }
+`;
