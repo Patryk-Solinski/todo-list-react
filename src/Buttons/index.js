9909d5cd9b0f5b556430e360e98ee2ff5460eada
Patryk Solinski <solinskipatryk8@gmail.com>
2023-02-01 18:25:16 +0100

    added styled-components

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,22 +1,23 @@
-import "./style.css";
+import { ListButtons, Button } from "./styled";
 
 const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) => (
 
-  <div className="buttons">
+  <ListButtons>
     {tasks.length > 0 && (
       <>
-        <button onClick={toggleHideDone} className="buttons__button">
+        <Button
+          onClick={toggleHideDone} >
           {hideDone ? "Pokaż ukończone" : "Ukryj ukończone"}
-        </button>
-        <button
+        </Button>
+        <Button
           onClick={setAllDone}
-          className="buttons__button"
-          disabled={tasks.every(({ done }) => done)}>
+          disabled={tasks.every(({ done }) => done)}
+          >
           Ukończ wszystkie
-        </button>
+        </Button>
       </>
     )}
-  </div>
+  </ListButtons>
 );
 
 
